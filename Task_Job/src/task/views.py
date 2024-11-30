58cd55ddedff2ef278101d68f68a2474433fd38b
Elsadany <Elsadany@gmail.com>
2024-11-30 21:25:20 +0200

    solving bug in naming

--- Task_Job/src/task/views.py
+++ Task_Job/src/task/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .form import PostTask
+from .form import postTask
 
 def post_task(request):
     if request.method == 'POST':
-        form = PostTask(request.POST, request.FILES)
+        form = postTask(request.POST, request.FILES)
         if form.is_valid():
             task = form.save(commit=False)
             task.owner = request.user
             task.save()
             return redirect('tasks:task_list')  # Update with an appropriate redirect
     else:
-        form = PostTask()
+        form = postTask()
     return render(request, 'task/postTask.html', {'form': form})
